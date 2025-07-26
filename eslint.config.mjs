f6993118accc44a1f4d43abc383bb6365b87d532
laurenzfiala <mail@laurenz.dev>
2025-07-26 12:36:28 +0200

    refactor(#106): add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,45 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import angular from 'angular-eslint';
+import jsdoc from 'eslint-plugin-jsdoc';
+
+export default tseslint.config(
+  {
+    files: ['**/*.ts'],
+    extends: [
+      eslint.configs.recommended,
+      ...tseslint.configs.recommended,
+      ...tseslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+    ],
+    plugins: { jsdoc },
+    processor: angular.processInlineTemplates,
+    rules: {
+      '@angular-eslint/directive-selector': [
+        'error',
+        {
+          type: 'attribute',
+          prefix: 'app',
+          style: 'camelCase',
+        },
+      ],
+      '@angular-eslint/component-selector': [
+        'error',
+        {
+          type: 'element',
+          prefix: 'app',
+          style: 'kebab-case',
+        },
+      ],
+      '@typescript-eslint/no-empty-function': 'off',
+      'jsdoc/no-undefined-types': 'error',
+    },
+  },
+  {
+    files: ['**/*.html'],
+    extends: [...angular.configs.templateRecommended, ...angular.configs.templateAccessibility],
+    rules: {
+      '@angular-eslint/template/attributes-order': 'error',
+    },
+  },
+);
